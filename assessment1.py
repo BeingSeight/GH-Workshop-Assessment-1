bc299c51d71bf2b81df2f2fbc3dd41bacbfe4596
Being Seight <beingseight@Being-Seights-Mac.local>
2025-01-25 14:43:57 +0530

    first commit

---
+++ assessment1.py
@@ -0,0 +1,44 @@
+# Make a program that prints about all of the terminal github
+# important commands and its uses
+
+# Importing the necessary libraries
+import os
+
+# Function to print the commands
+def print_commands():
+    print("Here are the important commands for github:")
+    print("1. git init: This command is used to initialize a new repository.")
+    print("2. git clone: This command is used to clone a repository from github.")
+    print("3. git add: This command is used to add files to the staging area.")
+    print("4. git commit: This command is used to commit changes to the repository.")
+    print("5. git push: This command is used to push changes to the remote repository.")
+    print("6. git pull: This command is used to pull changes from the remote repository.")
+    print("7. git status: This command is used to check the status of the repository.")
+    print("8. git log: This command is used to view the commit history.")
+    print("9. git branch: This command is used to manage branches in the repository.")
+    print("10. git merge: This command is used to merge changes from one branch to another.")
+    print("11. git checkout: This command is used to switch branches or restore files.")
+    print("12. git remote: This command is used to manage remote repositories.")
+    print("13. git fetch: This command is used to fetch changes from the remote repository.")
+    print("14. git reset: This command is used to reset changes in the repository.")
+    print("15. git stash: This command is used to stash changes in the repository.")
+    print("16. git tag: This command is used to manage tags in the repository.")
+    print("17. git diff: This command is used to view changes between commits.")
+    print("18. git rm: This command is used to remove files from the repository.")
+    print("19. git mv: This command is used to move or rename files in the repository.")
+    print("20. git blame: This command is used to view the author of a line in a file.")
+    print("21. git bisect: This command is used to find the commit that introduced a bug.")
+    print("22. git grep: This command is used to search the repository for a string.")
+    print("23. git reflog: This command is used to view the reference log.")
+    print("24. git clean: This command is used to remove untracked files from the repository.")
+    print("25. git config: This command is used to configure git settings.")
+
+# Main function
+def main():
+    print_commands()
+
+# Calling the main function
+if __name__ == "__main__":
+    main()
+
+# End of program
